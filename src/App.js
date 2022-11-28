430b7892e1ebf44bf603288f992f9b71573ec6c8
Chacheeokc <rgabrielchavez@outlook.com>
2022-11-27 20:42:41 -0600

    Component made and implemented, must be updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import GeoMap from "./components/GeoMap";
+import React from "react";
+
+const props = [
+  ["Country", "Popularity"],
+  ["Germany", 200],
+  ["United States", 300],
+  ["Brazil", 400],
+  ["Canada", 500],
+  ["France", 600],
+  ["RU", 700]
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Map </h1>
+        <div className="geoMap">
+          <GeoMap />
+        </div>
+       
       </header>
     </div>
   );
--- src/components/geoMap.js
+++ src/components/geoMap.js
@@ -1,8 +1,8 @@
-import React from "react";
 import Chart from "react-google-charts";
+import React, { Component } from "react"
 
 const data = [
-    ["Country", "Popularity"],
+    ["Country", "Visited"],
     ["Germany", 200],
     ["United States", 300],
     ["Brazil", 400],
@@ -10,10 +10,13 @@ const data = [
     ["France", 600],
     ["RU", 700]
   ];
-  export default class geoMap extends React.Component {
+  export default class GeoMap extends Component {
+   /* constructor(props) {
+      super(props)
+    }*/
     render() {
       return (
-        <div className="geoMap">
+        <div className="GeoMap">
           <Chart chartType="GeoChart" width="100%" height="400px" data={data} />
         </div>
       );
